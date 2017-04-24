e3707c3d7589a0f6bd0d9667027b6bc26038e6f9
Oscar Busk <oscar.busk@silobreaker.com>
2017-04-24 17:33:11 +0200

    Add SpacesModule

--- main.ts
+++ main.ts
@@ -1,6 +1,34 @@
-export * from './lib/spaces_base.service';
-export * from './lib/spaces_logging.service';
-export * from './lib/spaces_messages.service';
-export * from './lib/spaces_request.service';
-export * from './lib/spaces_storage.service';
-export * from './lib/spaces_utilities.service';
+import { NgModule } from '@angular/core';
+
+import { SpacesBaseService } from './lib/spaces_base.service';
+import { SpacesLoggingService } from './lib/spaces_logging.service';
+import { SpacesMessagesService } from './lib/spaces_messages.service';
+import { SpacesRequestService } from './lib/spaces_request.service';
+import { SpacesStorageService } from './lib/spaces_storage.service';
+import { SpacesUtilityService } from './lib/spaces_utilities.service';
+
+// Third part
+import { Device } from 'ng2-device-detector';
+
+@NgModule({
+    providers: [
+        SpacesBaseService,
+        SpacesLoggingService,
+        SpacesMessagesService,
+        SpacesRequestService,
+        SpacesStorageService,
+        SpacesUtilityService,
+        Device,
+    ]
+})
+class SpacesModule { }
+
+export {
+    SpacesBaseService,
+    SpacesLoggingService,
+    SpacesMessagesService,
+    SpacesRequestService,
+    SpacesStorageService,
+    SpacesUtilityService,
+    SpacesModule,
+}
